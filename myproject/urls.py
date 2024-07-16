735e1e7cae1e265d80c0f1066bf5c5f93486aab8
sumit01012001 <152839905+sumit01012001@users.noreply.github.com>
2024-07-16 12:43:42 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 ]
 
 if settings.DEBUG:
